18e52bf5e5167d2f304bc4a368c4e3a9e58bafff
qiudengqiang <qiudengqiang_i@didiglobal.com>
2021-08-02 15:55:18 +0800

    ADD - 死锁样例代码

---
+++ src/com/alphabethub/lock/DeadLockSample.java
@@ -0,0 +1,76 @@
+package com.alphabethub.lock;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 面试谈到死锁的部分时，有时候面试官会即兴让写一个死锁的例子
+ */
+public class DeadLockSample extends Thread {
+  private String first;
+  private String second;
+  public DeadLockSample(String name, String first, String second){
+      super(name);
+      this.first = first;
+      this.second = second;
+  }
+
+    @Override
+    public void run() {
+        synchronized (first){
+            System.out.println(this.getName()+" obtained："+first);
+            try {
+                Thread.sleep(1000L);
+                synchronized (second){
+                    System.out.println(this.getName()+" obtained："+second);
+                }
+            }catch (InterruptedException e){
+                // do nothing
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        // 扫描死锁程序
+//        findDeadLock();
+        // 死锁样例代码
+        String lockA = "lockA";
+        String lockB = "lockB";
+        DeadLockSample t1 = new DeadLockSample("Thread1",lockA,lockB);
+        //交换顺序才能导致死锁
+        DeadLockSample t2 = new DeadLockSample("Thread2",lockB,lockA);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+
+    /**
+     * Java提供的api，用于定位死锁
+     */
+    public static void findDeadLock(){
+        ThreadMXBean mbean = ManagementFactory.getThreadMXBean();
+        Runnable dlCheck = new Runnable() {
+
+            @Override
+            public void run() {
+                long[] threadIds = mbean.findDeadlockedThreads();
+                if (threadIds != null) {
+                    ThreadInfo[] threadInfos = mbean.getThreadInfo(threadIds);
+                    System.out.println("Detected deadlock threads:");
+                    for (ThreadInfo threadInfo : threadInfos) {
+                        System.out.println(threadInfo.getThreadName());
+                    }
+                }
+            }
+        };
+
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        // 稍等5秒，然后每10秒进行一次死锁扫描
+        scheduler.scheduleAtFixedRate(dlCheck, 5L, 10L, TimeUnit.SECONDS);
+    }
+}
